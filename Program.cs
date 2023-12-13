0ca7530650ac6d03b8ac62ab830a4cd069f3b4b2
Jakub Svoboda <jakub.svoboda@lego.com>
2023-12-13 15:51:33 +0100

    simple client promt

--- Program.cs
+++ Program.cs
@@ -4,14 +4,40 @@ using Azure.AI.OpenAI;
 using static System.Environment;
 using Microsoft.Extensions.Configuration;
 
-//Get appsettings
-var config = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .Build();
+// Build configuration & get data from json config
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        // Get values from appsettings.json
+        var openAIEndpoint = config.GetSection("OpenAIEndpoint").Value;
+        var key = config.GetSection("APIKey").Value;
 
 
-var endpoint = config.GetConnectionString("OpenAIEndpoint");
-var key = config.GetConnectionString("APIKey");
+//Interact with the user
+Console.WriteLine("Ask me a question and I will answer it!");
+var question = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(question))
+        {
+            Console.WriteLine("Please enter a valid question.");
+            return;
+        }
 
-var client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
-Console.WriteLine("Hello, World!");
+
+//Build message list
+List<ChatRequestUserMessage> questions = new List<ChatRequestUserMessage>();
+var acceptedQuestion = new ChatRequestUserMessage(question);
+questions.Add(acceptedQuestion);
+
+
+//Build the client & send request for completion
+var client = new OpenAIClient(new Uri(openAIEndpoint), new AzureKeyCredential(key));
+var options = new ChatCompletionsOptions("hackathon", questions){
+    MaxTokens = 50
+};
+var gptResponse = await client.GetChatCompletionsAsync(options);
+
+// Output the result  
+Console.WriteLine(gptResponse.Value.Choices[0].Message.Content);
+Console.WriteLine("this is it...");
