46a7b23c05cba328d48f40cd1e507aba3d6cfdfd
Jakub Svoboda <jakub.svoboda@lego.com>
2023-12-13 12:53:01 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure;
+using Azure.AI.OpenAI;
+using static System.Environment;
+using Microsoft.Extensions.Configuration;
+
+//Get appsettings
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+
+
+var endpoint = config.GetConnectionString("OpenAIEndpoint");
+var key = config.GetConnectionString("APIKey");
+
+var client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
+Console.WriteLine("Hello, World!");
